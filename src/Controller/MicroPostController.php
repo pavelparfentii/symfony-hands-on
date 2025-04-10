a6c7bdf046e35b737c1e8da5e916e07f9af1021f
Paul <pavel.parfentii@crisp-studio.com>
2025-04-10 16:39:32 +0300

    added user checker

--- src/Controller/MicroPostController.php
+++ src/Controller/MicroPostController.php
@@ -59,7 +59,7 @@ final class MicroPostController extends AbstractController
 
     #[Route('/micro-post/add', name: 'app_micro_post_add', priority: 2)]
 
-    #[IsGranted('IS_AUTHENTICATED_FULLY')]
+    #[IsGranted('ROLE_WRITER')]
     public function add(Request $request, EntityManagerInterface $entityManager):Response
     {
 //        $microPost = new MicroPost();
---
+++ src/Security/UserChecker.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Security;
+
+use Symfony\Component\Finder\Exception\AccessDeniedException;
+use Symfony\Component\Security\Core\User\UserCheckerInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+class UserChecker implements UserCheckerInterface{
+
+    /**
+     * @var \App\Entity\User $user
+    */
+    public function checkPreAuth(UserInterface $user){
+        if($user->getBannedUntil() === null){
+            return;
+        }
+
+        $now = new \DateTime();
+
+        if($now < $user->getBannedUntil()){
+            throw new AccessDeniedException('This user is banned.');
+        }
+    }
+    public function checkPostAuth(UserInterface $user){}
+}
