dadafea6e888a9b7b9e34a4c1f701eea3e31553d
Paul <pavel.parfentii@crisp-studio.com>
2025-04-15 17:37:14 +0300

    list of follower users

--- src/Controller/UserProfileController.php
+++ src/Controller/UserProfileController.php
@@ -16,4 +16,20 @@ final class UserProfileController extends AbstractController
             'user' => $user,
         ]);
     }
+
+    #[Route('/profile/{id}/follows', name: 'app_user_follows')]
+    public function follows(User $user): Response
+    {
+        return $this->render('user_profile/follows.html.twig', [
+            'user' => $user,
+        ]);
+    }
+
+    #[Route('/profile/{id}/followers', name: 'app_user_followers')]
+    public function followers(User $user): Response
+    {
+        return $this->render('user_profile/follower.html.twig', [
+            'user' => $user,
+        ]);
+    }
 }
